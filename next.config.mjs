d857b51508bb47cb178d2ffc4831aad0acc61278
syuchua <2102041310@st.nuc.edu.cn>
2024-11-01 23:15:06 +0800

    udate package.json

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import i18n from './next-i18next.config.js'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
+  },
+  i18n: i18n.i18n
+}
+
+export default nextConfig 
